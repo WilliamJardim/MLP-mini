9ffce448369cf4ad2aefff4d9a80d62edca43398
William Jardim <williamalvesjardim@gmail.com>
2024-10-18 23:02:03 -0300

    Aprimorei a função que criei para ajudar a detectar valores NaN

--- src/utils/debugIfSomeNaN.ts
+++ src/utils/debugIfSomeNaN.ts
@@ -1,19 +1,19 @@
 
-export default function debugIfSomeNaN( context:any, varToCheck:number[], callback:(resultV:any)=>void ): any{
+export default function debugIfSomeNaN( context:any, varToCheck:number[], callback?:(resultV:any)=>void ): any{
     let nanValues:number[] = [];
     let hasNaN:boolean = false;
 
     varToCheck.forEach( (val, valIndex)=>{
         if( isNaN(val) || !isFinite(val) ){
             nanValues.push( valIndex );
-            console.log( 'NaN', valIndex );
+            console.warn( 'NaN', valIndex, 'please insert debugger' );
             hasNaN = true;
         }
     });
 
     let result:any = { hasNaN: hasNaN, values: nanValues };
 
-    if(hasNaN){
+    if(hasNaN && callback){
         callback.bind(context)(result);
     }
 
